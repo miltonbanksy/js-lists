1f8b8b740ed0a2d7c0c423a43e3ef9520343cd03
miltonbanksy <miltonbanksy@gmail.com>
2026-01-27 14:44:49 +0900

    tidying and updating list options

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const inputTextToEdit = document.getElementById("input-text-to-edit");
 const outputText = document.getElementById("output-text");
 const btnListToJSON = document.getElementById('btn-list-to-json');
+const btnFormatToNewLines = document.getElementById('btn-format-to-new-lines');
 const btnRemoveNumbers = document.getElementById("btn-remove-numbers");
 const btnRemovePunctuation = document.getElementById("btn-remove-punctuation");
 const btnRemoveNumbersColonsSpaces = document.getElementById("btn-remove-numbers-colons-spaces");
-const btnConvertToArrayNewLineItems = document.getElementById("btn-convert-to-array-new-line-items");
 const btnCreateNewList = document.getElementById('btn-create-new-list');
 const listContainer = document.getElementById('list-container');
 
-// LISTENERS
+function displayOriginalText(text) {
+    outputText.value = text;
+}
+
+btnFormatToNewLines.addEventListener('click', () => {
+    let text = inputTextToEdit.value;
+    displayOriginalText(text);
+    let formattedText = text
+        .trim() // remove leading or trailing white spaces
+        .split(/\s+/)
+        .join('\n');
+
+    inputTextToEdit.value = formattedText;
+});
+
 btnCreateNewList.addEventListener('click', () => {
     let listTitle = prompt("Enter a title for the new list:", "List of ");
     
@@ -35,10 +49,11 @@ btnCreateNewList.addEventListener('click', () => {
 
 
 btnListToJSON.addEventListener('click', () => {
-    const listText = inputTextToEdit.value;
-    const listArray = listText.split('\n').filter(item => item.trim() !== '');
+    const text = inputTextToEdit.value;
+    displayOriginalText(text);
+    const listArray = text.split('\n').filter(item => item.trim() !== '');
     const jsonArrayString = JSON.stringify(listArray, null, 2);
-    outputText.value = jsonArrayString;
+    inputTextToEdit.value = jsonArrayString;
 });
 
 btnRemoveNumbers.addEventListener("click", () => {
@@ -54,23 +69,13 @@ btnRemovePunctuation.addEventListener("click", () => {
 });
 
 btnRemoveNumbersColonsSpaces.addEventListener('click', () => {
-    const originalString = inputTextToEdit.value; 
-    const originalString2 = removeNumbersColonsSpaces(originalString);
-    outputText.value = originalString2;
+    const text = inputTextToEdit.value;
+    outputText.value = text;
+    const originalString2 = removeNumbersColonsSpaces(text);
+    inputTextToEdit.value = originalString2;
 });
 
-btnConvertToArrayNewLineItems.addEventListener('click', () => {
-    const originalString = inputTextToEdit.value; 
-    const array = originalString
-        .split(/\r?\n/)
-        .map(item => item.trim())
-        .filter(item => item.length > 0 );
 
-    const result = JSON.stringify(array, null, 2);
-    outputText.value = result;
-});
-
-// HELPER FUNCTIONS
 function removeNumbersFromString(str) {
     return str.replace(/\d/g, '');
 };
